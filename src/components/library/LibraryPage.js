73737e6ad3beca761f44c21f34c1bfc2bc6ec051
Jasmina Gasper <jasminagasper@Jasminas-MacBook-Pro.local>
2018-03-14 14:12:40 +0000

    adding redux

--- src/components/library/LibraryPage.js
+++ src/components/library/LibraryPage.js
@@ -1,13 +1,69 @@
 import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as libraryActions from '../../actions/libraryActions';
 
 class LibraryPage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      book: { title: "" }
+    };
+
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+  }
+
+  onTitleChange(event) {
+    const book = this.state.book;
+    book.title = event.target.value;
+    this.setState({book: book});
+  }
+
+  onClickSave() {
+    this.props.actions.addBook(this.state.book);
+  }
+
+  bookRow (book, index) {
+    return <div key={index}>{book.title}</div>;
+  }
+
   render () {
     return (
       <div>
         <h1>Library</h1>
+        {this.props.library.map(this.bookRow)}
+        <h2>Add Book</h2>
+        <input
+          type="text"
+          onChange={this.onTitleChange}
+          value={this.state.book.title} />
+
+        <input
+          type="submit"
+          value="Save"
+          onClick={this.onClickSave} />
       </div>
     );
   }
 }
 
-export default LibraryPage;
+LibraryPage.propTypes = {
+  actions: PropTypes.object.isRequired,
+  library: PropTypes.array.isRequired
+};
+
+function mapStateToProps(state, ownProps) {
+  return {
+    library: state.library
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(libraryActions, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(LibraryPage);
---
+++ src/reducers/libraryReducer.js
@@ -0,0 +1,13 @@
+import * as types from '../actions/actionTypes';
+
+export default function libraryReducer(state = [], action) {
+  switch(action.type) {
+    case types.ADD_BOOK:
+      return [...state,
+        Object.assign({}, action.book)
+      ];
+
+    default:
+      return state;
+  }
+}
